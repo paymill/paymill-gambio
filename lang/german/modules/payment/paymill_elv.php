0fca16f7f37826e0106e83af98ef17b736dfaed2
kazemek <>
2013-10-21 10:37:17 +0200

    PM-739 rename "Elektronisches Lastschriftverfahren" => "ELV"

--- lang/german/modules/payment/paymill_elv.php
+++ lang/german/modules/payment/paymill_elv.php
@@ -1,6 +1,6 @@
 <?php
 define('TEXT_INFO_API_VERSION', 'API Version');
-define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_TITLE', ' Paymill Elektronische Lastschriftverfahren');
+define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_TITLE', ' PAYMILL ELV');
 define('MODULE_PAYMENT_PAYMILL_ELV_STATUS_TITLE', 'ELV Modul aktivieren');
 define('MODULE_PAYMENT_PAYMILL_ELV_STATUS_DESC', '');
 define('MODULE_PAYMENT_PAYMILL_ELV_TRANSACTION_ORDER_STATUS_ID_TITLE', 'Transaction Order Status');
@@ -28,11 +28,11 @@ define('MODULE_PAYMENT_PAYMILL_ELV_ALLOWED_TITLE' , 'Erlaubt f&uuml;r Zonen');
 define('MODULE_PAYMENT_PAYMILL_ELV_ALLOWED_DESC' , 'F&uuml;r alle Zonen leer lassen');
 define('MODULE_PAYMENT_PAYMILL_ELV_TRANS_ORDER_STATUS_ID_TITLE', 'API Ergebnisse');
 define('MODULE_PAYMENT_PAYMILL_ELV_TRANS_ORDER_STATUS_ID_DESC', 'API Ergebnisse in diesem Bestellstatus f&uuml;r Bestellungen speichern.');
-define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_PUBLIC_TITLE', 'Elektronische Lastschriftverfahren');
+define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_PUBLIC_TITLE', 'ELV');
 define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_ACCOUNT', 'Kontonummer');
 define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_BANKCODE', 'Bankleitzahl');
 define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_ACCOUNT_HOLDER', 'Kontoinhaber');
-define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_SAVED', 'Lastschriftverfahren powered by');
+define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_SAVED', 'ELV powered by');
 define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_ACCOUNT_INVALID', 'Die Kontonummer, die Sie angegeben haben, ist ungültig. Bitte korrigieren Sie Ihre Angaben.');
 define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_BANKCODE_INVALID', 'Die Bankleitzahl, die Sie angegeben haben, ist ungültig. Bitte korrigieren Sie Ihre Angaben.');
 define('MODULE_PAYMENT_PAYMILL_ELV_TEXT_ACCOUNT_HOLDER_INVALID', 'Der Kontoinhaber, die Sie angegeben haben, ist ungültig. Bitte korrigieren Sie Ihre Angaben.');
