73bab85565fc4cc0977f0cb6ac77c8179dd51d38
Thilo Hoffstadt <thilo.hoffstadt@payintelligent.de>
2014-01-20 17:11:14 +0100

    Fixed error message for invalid bic

--- ext/modules/payment/paymill/public/javascript/elv.js
+++ ext/modules/payment/paymill/public/javascript/elv.js
@@ -57,7 +57,7 @@ function PaymillValidateSepaForm(elvErrorFlag)
     }
 
     if(false === ($('#paymill-bic').val() != '')){
-        $('#elv-bic-error').text(elv_iban_invalid);
+        $('#elv-bic-error').text(elv_bic_invalid);
         $('#elv-bic-error').css('display', 'block');
         elvErrorFlag = false;
     }
